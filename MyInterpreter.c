fd824b37532bc402eb53c76e917f9abdb2239b82
Han ChengYi <1332417183@qq.com>
2019-10-29 02:05:10 +0800

    Second commit

--- MyInterpreter.c
+++ MyInterpreter.c
@@ -8,14 +8,31 @@ int assembly; //汇编模式
 int debug;    //调试模式
 
 int main(int argc, char** argv){
-    
+    int fd; // 文件描述符
+
     // 解析输入参数
-    // -s
+    // 如果第1个参数: -s
     if(argc > 0 && **argv == '-' && (*argv)[1] == 's'){
         assembly = 1;
         argc--;
         argv++;
     }
-    // -d
-
+    // 如果第1个参数: -d
+    if(argc > 0 && **argv == '-' && (*argv)[1] == 'd'){
+        debug = 1;
+        argc--;
+        argv++;
+    }
+    // 如果没有带输入参数
+    if (argc < 1)
+    {
+        printf("Usage: MyInterpreter [-s] [-d] file ...\n");
+    }
+    // 如果文件打开失败
+    if((fd = open(*argv,0)) < 0){
+        prinf("Couldn't open the file (%s)\n",*argv);
+        return -1;
+    }
+    // 如果文件打开成功
+    
 }
